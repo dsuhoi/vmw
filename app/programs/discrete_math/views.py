ff3782ad954a14c0feeeada263aa8d4f760ef70b
dsuhoi <dsuh0i.h8@gmail.com>
2021-11-19 14:46:01 +0300

    Update for merge.

--- app/programs/discrete_math/views.py
+++ app/programs/discrete_math/views.py
@@ -2,7 +2,7 @@ from . import discrete_math as dscm
 from app.programs.utils import render_decorator
 from flask import request, render_template
 import mpld3
-from .modules import graph
+from .modules import graph as graph_module
 
 # График для генерации
 g_result_html = None
@@ -24,11 +24,11 @@ def graphs():
         config['iframe'] = True
         text = None
         if task=='create':
-            figure, text = graph.create(params)
+            figure, text = graph_module.create(params)
         elif task=='planar':
-            figure, text = graph.planar(params)
+            figure, text = graph_module.planar(params)
         elif task=='chromatic':
-            figure, text = graph.chromatic(params)
+            figure, text = graph_module.chromatic(params)
         g_result_html = mpld3.fig_to_html(figure)
         return text
     return function()
