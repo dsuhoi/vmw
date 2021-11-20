c9996fc35c213f3f99fdd8beaa3d05cae33e005c
h3avenqq <shmarev082@gmail.com>
2021-11-20 17:46:41 +0300

    tiny fix

--- app/programs/linalg/views.py
+++ app/programs/linalg/views.py
@@ -1,13 +1,12 @@
 from . import linalg as lng
 from app.programs.utils import render_decorator
 from flask import request, render_template
-import mpld3
 
 @lng.route('/')
 def index():
     return render_template('linalg.html')
 
-@lng.route('/invmatrix', methods=['GET'])
+@lng.route('matrix', methods=['GET'])
 def matrix():
     @render_decorator('matrix.html', ['matrix'])
     def function(task, params, config):
