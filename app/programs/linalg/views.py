53a71e21aaea52a2c459b019eff1c43c9c149951
h3avenqq <shmarev082@gmail.com>
2021-11-20 17:39:33 +0300

    Added linalg-matrix page without calculation alghorithms

--- app/programs/linalg/views.py
+++ app/programs/linalg/views.py
@@ -1,34 +1,16 @@
-from . import discrete_math as dscm
+from . import linalg as lng
 from app.programs.utils import render_decorator
 from flask import request, render_template
 import mpld3
-from .modules import graph
 
-# График для генерации
-g_result_html = None
-
-@dscm.route('/result')
-def result():
-    global g_result_html
-    return g_result_html
-
-@dscm.route('/')
+@lng.route('/')
 def index():
-    return render_template('discrete_math.html')
+    return render_template('linalg.html')
 
-@dscm.route('/graphs', methods=['GET'])
-def graphs():
-    @render_decorator('graphs.html', ['matrix'])
+@lng.route('/invmatrix', methods=['GET'])
+def matrix():
+    @render_decorator('matrix.html', ['matrix'])
     def function(task, params, config):
-        global g_result_html
-        config['iframe'] = True
-        text = None
-        if task=='create':
-            figure, text = graph.create(params)
-        elif task=='planar':
-            figure, text = graph.planar(params)
-        elif task=='chromatic':
-            figure, text = graph.chromatic(params)
-        g_result_html = mpld3.fig_to_html(figure)
+        text = ''
         return text
     return function()
