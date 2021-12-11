b028c439ff3bf45f218da0001c74c16a1b27718b
dsuhoi <dsuh0i.h8@gmail.com>
2021-12-11 20:00:54 +0300

    Added instructions.

--- app/post/views.py
+++ app/post/views.py
@@ -5,7 +5,7 @@ from flask import Flask, request, render_template, redirect, url_for
 @post.route('/')
 def index():
     articles = Articles.query
-    chapters = articles.with_entities(Articles.chapter).distinct()
+    chapters = articles.filter(Articles.chapter!="#").with_entities(Articles.chapter).distinct()
     articles_dict = \
         {chapt[0]: articles.filter(Articles.chapter==chapt[0]).all() for chapt in chapters}
     return render_template('post.html', post_dict=articles_dict)
--- app/programs/utils.py
+++ app/programs/utils.py
@@ -1,8 +1,12 @@
 from flask import request, render_template
+from os import path
+from app.models import Articles
 
 def render_decorator(render_file, arg_list):
     def decorator(func):
         def wrapper():
+            file_name, _ = path.splitext(render_file)
+            instruction = Articles.query.filter(Articles.title==file_name).first()
             result = None
             try:
                 task_get = request.args.get('task_list')
@@ -10,9 +14,10 @@ def render_decorator(render_file, arg_list):
                 if params[arg_list[0]] and params[arg_list[0]]!='':
                     result = {}
                     result['text'] = func(task_get, params, result)
+                    
             except Exception as e:
-                return render_template(render_file, params=params, error=e.__str__())
+                return render_template(render_file, params=params, error=e.__str__(), instruction=instruction)
             else:
-                return render_template(render_file, params=params, result=result)
+                return render_template(render_file, params=params, result=result, instruction=instruction)
         return wrapper
     return decorator
