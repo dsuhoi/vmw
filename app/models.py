ff56098e4716e155764a44ef945059d20ac529d1
dsuhoi <dsuh0i.h8@gmail.com>
2022-07-17 16:00:27 +0700

    Fixed __init__ files.

--- app/models.py
+++ app/models.py
@@ -2,7 +2,6 @@ from app import db
 
 
 class Articles(db.Model):
-    # __tablename__ == 'articles'
     id = db.Column(db.Integer(), nullable=False, primary_key=True)
     title = db.Column(db.Text(), nullable=False)
     chapter = db.Column(db.Text(), nullable=False)
--- app/programs/linalg/__init__.py
+++ app/programs/linalg/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 
-linalg = Blueprint('linalg', __name__,
-        template_folder='templates', url_prefix='/linalg')
+linalg = Blueprint(
+    "linalg", __name__, template_folder="templates", url_prefix="/linalg"
+)
 
 from . import views
