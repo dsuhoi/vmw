ed6bd649d98ee156da470d99de6bf46d18250cd6
dsuhoi <dsuh0i.h8@gmail.com>
2022-11-25 13:25:07 +0700

    Added database tests.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 import pytest
 from app import create_app
+from app.models import Articles
 
 
 @pytest.fixture(scope="module")
@@ -21,3 +22,10 @@ def test_check_page(client, url, result):
     resp = client.get(url, follow_redirects=True)
     assert resp.status_code == 200
     assert result in resp.data.decode("utf-8")
+
+
+@pytest.mark.parametrize(
+    "title, result", [("deriv", "$z(x, y), x y$"), ("diff", "$y'(y(x), x)$")]
+)
+def test_database(title, result):
+    assert result in Articles.query.filter(Articles.title == title).all()[0].content
